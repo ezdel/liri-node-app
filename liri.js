579a8cb7671ce688533428624dc6535edd4eaa64
Eric Zuckerman <ericzuckerman@Erics-MBP.att.net>
2016-09-06 13:08:11 -0700

    Added log. Everything working except do-what-it-says.

--- liri.js
+++ liri.js
@@ -4,7 +4,8 @@ var spotify = require ('spotify');
 var request = require('request');
 var fs = require('fs');
 
-var method = process.argv[2];
+var method = '';
+var name = '';
 var nodeArgs= process.argv;
 
 
@@ -15,6 +16,19 @@ var client = new twitter({
 	access_token_secret: keys.twitterKeys.access_token_secret
 });
 
+if (process.argv[2] === 'do-what-it-says'){
+	fs.readFile('random.txt', 'utf8', function(err, data){
+		var dataArray = data.split(',');
+		method = dataArray[0];
+		name = dataArray[1];
+		console.log(method);
+		console.log(name);
+	});
+}
+else {
+	method = process.argv[2];
+};
+
 if (method === 'my-tweets'){
 	var twitterparams = {screen_name: 'EricTheZuck', count: '20', trim_user: true};
 	client.get('statuses/user_timeline', twitterparams, function(error, tweets, response){
@@ -24,24 +38,22 @@ if (method === 'my-tweets'){
 				console.log(tweets[i].text);
 				console.log(tweets[i].created_at);
 				console.log('---------------------');
+				fs.appendFile('log.txt', tweets[i].text + ' ' + tweets[i].created_at + ' ');
+				
 			};	
-		}
-		else{
-			console.log(error);
 		};
 	});
 };
 
 if (method === 'spotify-this-song'){
 	var songArgs = nodeArgs.slice(3);
-	var songName = '';  
 	if (songArgs.length < 1){
-		songName = "All That She Wants";
+		name = "All That She Wants";
 	}
 	else{
-		songName = songArgs.join(' ');
+		name = songArgs.join(' ');
 	};
-	spotify.search({type: 'track', query: songName}, function(err, data){
+	spotify.search({type: 'track', query: name}, function(err, data){
 		if (err){
 			console.log(err);
 		}
@@ -51,25 +63,25 @@ if (method === 'spotify-this-song'){
 			console.log('Preview Link: ' + data.tracks.items[0].external_urls.spotify);
 			console.log('Album: ' + data.tracks.items[0].album.name);
 			console.log('--------------------------------')
+			fs.appendFile('log.txt', 'Song: ' + data.tracks.items[0].name + ' ' + 'Artist: ' + data.tracks.items[0].artists[0].name + ' ' + 'Preview Link: ' + data.tracks.items[0].external_urls.spotify + ' ' + 'Album: ' + data.tracks.items[0].album.name + ' ');
 		};
 	});
 };
 
 if (method === 'movie-this'){
 	var movieArgs = nodeArgs.slice(3);
-	var movieName = '';
 	if (movieArgs.length < 1){
-		movieName = "Mr. Nobody"
+		name = "Mr. Nobody"
 	};
 	for (i=0; i<movieArgs.length; i++){
 		if (i>0 && i<movieArgs.length){
-			movieName = movieName + '+' + movieArgs[i];
+			name = name + '+' + movieArgs[i];
 		}
 		else{
-			movieName = movieName + movieArgs[i];
+			name = name + movieArgs[i];
 		};
 	};
-	request('http://www.omdbapi.com/?t=' + movieName + '&y=&plot=short&tomatoes=true&r=json', function (error, response, body) {
+	request('http://www.omdbapi.com/?t=' + name + '&y=&plot=short&tomatoes=true&r=json', function (error, response, body) {
 		if (!error && response.statusCode == 200) {
 			console.log('---------- ' + JSON.parse(body)["Title"] + ' ----------')
 			console.log('Year: ' + JSON.parse(body)["Year"]);
@@ -81,17 +93,8 @@ if (method === 'movie-this'){
 			console.log('Tomato Meter: ' + JSON.parse(body)["tomatoMeter"]);
 			console.log('Rotten Tomatoes URL: ' + JSON.parse(body)["tomatoURL"]);
 			console.log('-------------------------------------')
-
+			fs.appendFile('log.txt', 'Title: ' + JSON.parse(body)["Title"] + ' ' + 'Year: ' + JSON.parse(body)["Year"] + ' ' + 'IMDB Rating: ' + JSON.parse(body)["imdbRating"] + ' ' + 'Country: ' + JSON.parse(body)["Country"] + ' ' + 'Language: ' + JSON.parse(body)["Language"] + ' ' + 'Plot: ' + JSON.parse(body)["Plot"] + ' ' + 'Actors: ' + JSON.parse(body)["Actors"] + ' ' + 'Tomato Meter: ' + JSON.parse(body)["tomatoMeter"] + ' ' + 'Rotten Tomatoes URL: ' + JSON.parse(body)["tomatoURL"] + ' ');
 		};
 	});
-};
-
-if (method === 'do-what-it-says'){
-	fs.readFile('random.txt', 'utf8', function(err, data){
-	
-	var dataArray = data.split(',');
-	method = dataArray[0];
-	songName = dataArray[1];
-});
 };
 
